b832121a8ae4a90bbd4419a8ffc7897770fbd084
chengjie_huang <chengjie_huang@chengjie-huangs-MacBook-Air.local>
2022-11-01 23:40:57 -0400

    prob: lc blind 75 - finish binary search problems

---
+++ binarySearch/kokoEatingBananas.py
@@ -0,0 +1,26 @@
+# 875. Koko Eating Bananas https://leetcode.com/problems/koko-eating-bananas/
+
+
+# Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+
+# Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+# Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+# Return the minimum integer k such that she can eat all the bananas within h hours.
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left = 1
+        right = max(piles)
+
+        while left <= right:
+            mid = (left + right) // 2
+            count = sum([math.ceil(pile / mid) for pile in piles])
+
+            if count > h:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return left
